7959c0ed64a7a7facfbf19fd2f7bb4319d8794c7
abeja-yuki <yuki@abeja.asia>
2018-02-16 14:28:58 +0900

    files for package management

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+requires = open('requirements.txt').read()
+
+setup(
+    name='pandas_bj',
+    version='0.0.1',
+    packages=['pandas_bj'],
+    url='https://github.com/abeja-yuki/pandas_bj',
+    license='MIT',
+    author='Yuki Chiba',
+    author_email='yuki.music.0283@gmail.com',
+    description='Pandas BJ (Between Join) provides efficient way to use `JOIN` and `BETWEEN` comparison. with pandas.DataFrame.',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=requires,
+)
